449d07aae725ae6c35cad52ae71a633ec76a4774
Igor Vinícius <igorvinnicius@gmail.com>
2020-12-10 19:29:48 +0100

    added create post validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,13 +1,29 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: "First Post", content: "This is the first post!"}]
+        posts: [
+            {
+                title: "First Post", 
+                content: "This is the first post!",
+                imageUrl: 'images/duck.jpg'
+            }
+        ]
     });
 };
 
 exports.createPost = (req, res, next) => {
     
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        res.status(422).json({message: 'Validation failed!', errors: errors.array()});
+    }
+
     const title = req.body.title;
     const content = req.body.content;
+
+    console.log(title, content);
 
     res.status(201).json({
         message: "Post created successfully!",
