e0ccc2c28024b3d840466aa9c3e045726bb6f3e5
Igor Vinícius <igorvinnicius@gmail.com>
2020-12-18 18:35:44 +0100

    using stubs

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,46 +40,48 @@ exports.signup = (req, res, next) => {
 
 };
 
-exports.login = (req, res, next) => {
-    
+exports.login = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
+    
+    try {
+      const user = await User.findOne({ email: email });
+      
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
 
-    User.findOne({email: email})
-        .then(user => {
+      loadedUser = user;
 
-            if(!user){
-                const error = new Error('User email not found!');
-                error.statusCode = 401;
-                throw error;
-            }
+      const isEqual = await bcrypt.compare(password, user.password);
+      
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
 
-            loadedUser = user;
-            return bcrypt.compare(password, user.password);
-        })
-        .then(isEqual => {
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'somesupersecretsecret',
+        { expiresIn: '1h' }
+      );
 
-            if(!isEqual){
-                const error = new Error('Wrong password!');
-                error.statusCode = 401;
-                throw error;
-            }
-
-            const token = jwt.sign({
-                email: loadedUser.email,
-                userId: loadedUser._id.toString()
-            }, 
-            'somesupersecretsecret',
-            { expiresIn: '1h' });
-
-            res.status(200).json({ token: token, userId: loadedUser._id.toString() });
-
-        })
-        .catch(err => {
-        if(!err.statusCode){
-            err.statusCode = 500;
-        }
-        next(err)
-    });
-};
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+      return;
+    } catch (err) {
+      
+        if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      
+      next(err);
+      return err;
+    }
+  };
